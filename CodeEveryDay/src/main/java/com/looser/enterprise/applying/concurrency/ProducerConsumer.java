66d2309d30829adbbc49edc4a093e9e939e8e625
SAMAGRA17KASHYAP <kashyap.samagraa@gmail.com>
2019-02-23 23:04:25 +0530

    Changes for the wait notify pattern

--- CodeEveryDay/src/main/java/com/looser/enterprise/applying/concurrency/ProducerConsumer.java
+++ CodeEveryDay/src/main/java/com/looser/enterprise/applying/concurrency/ProducerConsumer.java
@@ -1,37 +1,76 @@
 package com.looser.enterprise.applying.concurrency;
 
 public class ProducerConsumer {
-	int count = 0;
-	int[] buffer = new int[100];
+	private static int count = 0;
+	private static int[] buffer = new int[100];
 
-	public Object lock = new Object();
+	public static Object lock = new Object();
 
-	class Producer {
-		public void produce() {
+	static class Producer {
+		public void produce() throws InterruptedException {
 			synchronized (lock) {
-				while (isFull())
-					;
+				if (isFull())
+					lock.wait();
 				buffer[count] = 1;
 				count++;
+				lock.notifyAll();
 			}
 		}
 
-		private boolean isFull() {
-			return buffer.length == count;
-		}
 	}
 
-	class Consumer {
-		public void consume() {
+	private static boolean isFull() {
+		return buffer.length == count;
+	}
+
+	static class Consumer {
+		public void consume() throws InterruptedException {
 			synchronized (lock) {
-				while (isEmpty())
-					;
+				if (isEmpty())
+					lock.wait();
 				buffer[--count] = 0;
+				lock.notifyAll();
 			}
 		}
 
-		private boolean isEmpty() {
-			return 0 == count;
-		}
 	}
+
+	private static boolean isEmpty() {
+		return 0 == count;
+	}
+
+	public static void main(String[] args) throws Exception {
+		Producer producer = new Producer();
+		Consumer consumer = new Consumer();
+		Runnable producerTask = () -> {
+			for (int i = 0; i < 100; i++) {
+				try {
+					producer.produce();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+			System.out.println("Done producer...");
+		};
+		Runnable consumerTask = () -> {
+			for (int i = 0; i < 100; i++) {
+				try {
+					consumer.consume();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.println("Done consuming...");
+		};
+		Thread t1 = new Thread(producerTask);
+		Thread t2 = new Thread(consumerTask);
+		t1.start();
+		t2.start();
+		t1.join();
+		t2.join();
+		System.out.println("Count::"+count);
+	}
+
 }
