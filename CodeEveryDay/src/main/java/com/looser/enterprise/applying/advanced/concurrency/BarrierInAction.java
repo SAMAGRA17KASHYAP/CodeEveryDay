aeba18ae612f4353428180a21d2659b06bf4742e
SAMAGRA17KASHYAP <kashyap.samagraa@gmail.com>
2019-02-25 02:06:54 +0530

    Handling  barrier using timeout of get of future

--- CodeEveryDay/src/main/java/com/looser/enterprise/applying/advanced/concurrency/BarrierInAction.java
+++ CodeEveryDay/src/main/java/com/looser/enterprise/applying/advanced/concurrency/BarrierInAction.java
@@ -1,6 +1,7 @@
 package com.looser.enterprise.applying.advanced.concurrency;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Callable;
@@ -10,26 +11,37 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class BarrierInAction {
 	
 	public static void main(String[] args) throws InterruptedException {
 		ExecutorService service = Executors.newFixedThreadPool(2);//Note now barrier will never open as thread count is less
-		List<Friend> friends = new ArrayList<>();
-		CyclicBarrier cyclicBarrier = new CyclicBarrier(4,()->System.out.println("Latch temporaily opened"));
-		friends.add(new Friend(cyclicBarrier));
-		friends.add(new Friend(cyclicBarrier));
-		friends.add(new Friend(cyclicBarrier));
-		friends.add(new Friend(cyclicBarrier));
-		List<Future<String>> result = service.invokeAll(friends);
-		result.forEach(x->{
-			try {
-				x.get();
-			} catch (Exception e) {
-				System.out.println(e);;
+		try {
+			List<Friend> friends = new ArrayList<>();
+			CyclicBarrier cyclicBarrier = new CyclicBarrier(4, () -> System.out.println("Latch temporaily opened"));
+			friends.add(new Friend(cyclicBarrier));
+			friends.add(new Friend(cyclicBarrier));
+			friends.add(new Friend(cyclicBarrier));
+			friends.add(new Friend(cyclicBarrier));
+			List<Future<String>> result = new ArrayList<>();
+			for (Friend friend : friends) {
+				result.add(service.submit(friend));
 			}
-		});
-		service.shutdown();
+			result.forEach(x -> {
+				try {
+					System.out.println(x.get(1, TimeUnit.MICROSECONDS));
+				} catch (TimeoutException e) {
+					System.out.println("Timeout...");
+					x.cancel(true);
+				} catch (Exception e) {
+					System.out.println(e);
+					;
+				}
+			});
+		} finally {
+			service.shutdown();
+		}
 
 	}
 	static class Friend implements Callable<String>
@@ -50,9 +62,9 @@ public class BarrierInAction {
 			try
 			{
 				Random rand = new Random();
-				Thread.sleep(rand.nextInt(1_0) *100);
+				Thread.sleep(rand.nextInt(10) *10);
 				System.out.println("Reached cafe...");
-				cyclicBarrier.await(10,TimeUnit.SECONDS);
+				cyclicBarrier.await();
 					
 				System.out.println("Let us go to cinema...");
 			}
