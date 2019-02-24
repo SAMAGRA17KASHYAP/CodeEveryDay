02537a2d8db5843e8ef00067a120c73e9edbcd57
SAMAGRA17KASHYAP <kashyap.samagraa@gmail.com>
2019-02-24 15:04:58 +0530

    Showing difference between running new Thread each time versus using a Thread Pool

---
+++ CodeEveryDay/src/main/java/com/looser/enterprise/applying/advanced/concurrency/DemoExecutors.java
@@ -0,0 +1,31 @@
+package com.looser.enterprise.applying.advanced.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class DemoExecutors {
+	public static void main(String[] args) {
+		runningNewThreadEveryTime();
+		System.out.println("=======================================");
+		runningTaskAsyncUsingExecutor();
+	}
+
+	private static void runningTaskAsyncUsingExecutor() {
+		Runnable runnable = ()-> {
+			System.out.println("From runnable:"+Thread.currentThread().getName());
+		};
+		ExecutorService service = Executors.newSingleThreadExecutor();
+		for (int i = 0; i < 10; i++) {
+			service.execute(runnable);
+		}
+	}
+
+	private static void runningNewThreadEveryTime() {
+		Runnable runnable = ()-> {
+			System.out.println("From runnable:"+Thread.currentThread().getName());
+		};
+		for (int i = 0; i < 10; i++) {
+			new Thread(runnable).start();
+		}
+	}
+}
