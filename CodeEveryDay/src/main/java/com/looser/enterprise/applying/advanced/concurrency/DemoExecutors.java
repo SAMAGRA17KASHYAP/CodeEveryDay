44d18db3c924ff779b8ee4f7d9dd98af738ac2a0
SAMAGRA17KASHYAP <kashyap.samagraa@gmail.com>
2019-02-24 15:11:47 +0530

    Fixed Size Thread Pool Demo

--- CodeEveryDay/src/main/java/com/looser/enterprise/applying/advanced/concurrency/DemoExecutors.java
+++ CodeEveryDay/src/main/java/com/looser/enterprise/applying/advanced/concurrency/DemoExecutors.java
@@ -8,8 +8,20 @@ public class DemoExecutors {
 		runningNewThreadEveryTime();
 		System.out.println("=======================================");
 		runningTaskAsyncUsingExecutor();
+		System.out.println("=======================================");
+		runningTaskAsyncUsingFixedSizeExecutor();
 	}
 
+	private static void runningTaskAsyncUsingFixedSizeExecutor() {
+		Runnable runnable = ()-> {
+			System.out.println("From runnable:"+Thread.currentThread().getName());
+		};
+		ExecutorService service = Executors.newFixedThreadPool(3);
+		for (int i = 0; i < 10; i++) {
+			service.execute(runnable);
+		}
+		service.shutdown();
+	}
 	private static void runningTaskAsyncUsingExecutor() {
 		Runnable runnable = ()-> {
 			System.out.println("From runnable:"+Thread.currentThread().getName());
