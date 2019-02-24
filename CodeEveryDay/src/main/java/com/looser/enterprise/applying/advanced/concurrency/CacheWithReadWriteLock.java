34d9f8d1bd5a056c3f085d697eff00739d1fc4ef
SAMAGRA17KASHYAP <kashyap.samagraa@gmail.com>
2019-02-25 00:32:30 +0530

    Cache works properly with read write lock

--- CodeEveryDay/src/main/java/com/looser/enterprise/applying/advanced/concurrency/CacheWithReadWriteLock.java
+++ CodeEveryDay/src/main/java/com/looser/enterprise/applying/advanced/concurrency/CacheWithReadWriteLock.java
@@ -6,19 +6,38 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class CacheWithReadWriteLock {
 
 	Map<Long,String> cache = new HashMap<>();
 	
+	ReadWriteLock readWriteLock  = new ReentrantReadWriteLock();	  
+	Lock read = readWriteLock.readLock();
+	Lock write = readWriteLock.writeLock();
+	
 	
 	public void put(Long key, String value) {
-		cache.put(key, value);
+		try {
+			write.lock();
+			cache.put(key, value);
+		} finally {
+			write.unlock();
+		}
 	}
 	
 	public String get(Long key)
 	{
-		return cache.get(key);
+		try {
+			read.lock();
+			return cache.get(key);
+		}
+		finally {
+			read.unlock();
+		}
+		
 	}
 	
 	public static void main(String[] args) {
