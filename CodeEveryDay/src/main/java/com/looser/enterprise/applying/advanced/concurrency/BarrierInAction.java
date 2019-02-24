e6fca72e347d19334b4e7fed43d27ec2e54e9625
SAMAGRA17KASHYAP <kashyap.samagraa@gmail.com>
2019-02-25 01:47:24 +0530

    handling when barrier could not be opened

--- CodeEveryDay/src/main/java/com/looser/enterprise/applying/advanced/concurrency/BarrierInAction.java
+++ CodeEveryDay/src/main/java/com/looser/enterprise/applying/advanced/concurrency/BarrierInAction.java
@@ -5,20 +5,30 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class BarrierInAction {
 	
 	public static void main(String[] args) throws InterruptedException {
-		ExecutorService service = Executors.newFixedThreadPool(4);
+		ExecutorService service = Executors.newFixedThreadPool(2);//Note now barrier will never open as thread count is less
 		List<Friend> friends = new ArrayList<>();
 		CyclicBarrier cyclicBarrier = new CyclicBarrier(4,()->System.out.println("Latch temporaily opened"));
 		friends.add(new Friend(cyclicBarrier));
 		friends.add(new Friend(cyclicBarrier));
 		friends.add(new Friend(cyclicBarrier));
 		friends.add(new Friend(cyclicBarrier));
-		service.invokeAll(friends);
+		List<Future<String>> result = service.invokeAll(friends);
+		result.forEach(x->{
+			try {
+				x.get();
+			} catch (Exception e) {
+				System.out.println(e);;
+			}
+		});
 		service.shutdown();
 
 	}
@@ -42,11 +52,12 @@ public class BarrierInAction {
 				Random rand = new Random();
 				Thread.sleep(rand.nextInt(1_0) *100);
 				System.out.println("Reached cafe...");
-				cyclicBarrier.await();
+				cyclicBarrier.await(10,TimeUnit.SECONDS);
+					
 				System.out.println("Let us go to cinema...");
 			}
 			catch (Exception e) {
-				System.out.println(e.getMessage());
+				System.out.println(e);
 			}
 			return "ok";
 		}
