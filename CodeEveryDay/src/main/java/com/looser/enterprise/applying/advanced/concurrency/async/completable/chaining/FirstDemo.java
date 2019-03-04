c6476c504276fddef7224e4968e4a937685879eb
SAMAGRA17KASHYAP <kashyap.samagraa@gmail.com>
2019-03-05 00:07:53 +0530

    With external thread pool

--- CodeEveryDay/src/main/java/com/looser/enterprise/applying/advanced/concurrency/async/completable/chaining/FirstDemo.java
+++ CodeEveryDay/src/main/java/com/looser/enterprise/applying/advanced/concurrency/async/completable/chaining/FirstDemo.java
@@ -3,6 +3,8 @@ package com.looser.enterprise.applying.advanced.concurrency.async.completable.ch
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -23,23 +25,27 @@ public class FirstDemo {
 	
 	public static void main(String[] argv)
 	{
+		ExecutorService service =Executors.newFixedThreadPool(1);
 		Supplier<List<Long>>  supplyIDs = ()->{
 			sleep(500);
 			return Arrays.asList(1L,2L,3L);
 		};
 		
 		Function<List<Long>,List<User>> fetchUsers = ids->{
-			sleep(300);
+			sleep(800);
 			return ids.stream().map(x->new User(x)).collect(Collectors.toList());
 		};
 		
-		Consumer<List<User>> displayer = users -> users.forEach(System.out::println);
+		Consumer<List<User>> displayer = users -> users.forEach(x->{
+			System.out.println("Running in ::"+Thread.currentThread().getName());
+			System.out.println(x);});
 		
 		CompletableFuture<Void> completableFuture = CompletableFuture.supplyAsync(supplyIDs)
 							.thenApply(fetchUsers)
-							.thenAccept(displayer);
+							.thenAcceptAsync(displayer,service);
 		
-		sleep(1_000);
+		sleep(2_000);
+		service.shutdown();
 	}
 	
 }
